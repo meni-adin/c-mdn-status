0365b66417a42e5314489cbd42333b9b76a5ecdc
Menachem Adin <67019692+meni-adin@users.noreply.github.com>
2024-09-22 18:06:41 +0300

    Add sanitizers check (#10)

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,3 +1,18 @@
+function(set_compiler_flags)
+    if((CMAKE_C_COMPILER_ID STREQUAL "AppleClang") OR (CMAKE_C_COMPILER_ID STREQUAL "GNU"))
+        if(CMAKE_C_COMPILER_ID STREQUAL "AppleClang")
+        endif()
+        if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
+        endif()
+    elseif(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+        add_compile_options(
+            /wd5072
+        )
+else()
+        message(FATAL_ERROR "Unknown C compiler: ${CMAKE_C_COMPILER_ID}")
+    endif()
+endfunction()
+
 function(set_target_c_compiler_flags target)
     if((CMAKE_C_COMPILER_ID STREQUAL "AppleClang") OR (CMAKE_C_COMPILER_ID STREQUAL "GNU"))
         target_compile_options(${target} PRIVATE
@@ -25,9 +40,61 @@ function(set_target_c_compiler_flags target)
             /Wall
             /WX
             /wd4710
+            /wd4711
             /wd5045
+            /wd5072
         )
 else()
         message(FATAL_ERROR "Unknown C compiler: ${CMAKE_C_COMPILER_ID}")
+    endif()
+endfunction()
+
+function(set_target_cpp_compiler_flags target)
+    if((CMAKE_C_COMPILER_ID STREQUAL "AppleClang") OR (CMAKE_C_COMPILER_ID STREQUAL "GNU"))
+        if(CMAKE_C_COMPILER_ID STREQUAL "AppleClang")
+        endif()
+        if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
+        endif()
+    elseif(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+        target_compile_options(${target} PRIVATE
+            /Wall
+            /WX
+            /wd4626
+            /wd4710
+            /wd4711
+            /wd5027
+            /wd5072
+        )
+    else()
+        message(FATAL_ERROR "Unknown C compiler: ${CMAKE_C_COMPILER_ID}")
+    endif()
+endfunction()
+
+function(enable_sanitizers)
+    if((CMAKE_C_COMPILER_ID STREQUAL "AppleClang") OR (CMAKE_C_COMPILER_ID STREQUAL "GNU"))
+        add_compile_options(-fsanitize=address -fno-omit-frame-pointer)
+        add_link_options(-fsanitize=address)
+    elseif(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+        add_compile_options(/fsanitize=address)
+        add_link_options(/fsanitize=address)
+
+        foreach(flag_var
+            CMAKE_C_FLAGS
+            CMAKE_C_FLAGS_DEBUG
+            CMAKE_C_FLAGS_RELEASE
+            CMAKE_C_FLAGS_RELWITHDEBINFO
+            CMAKE_C_FLAGS_MINSIZEREL
+            CMAKE_CXX_FLAGS
+            CMAKE_CXX_FLAGS_DEBUG
+            CMAKE_CXX_FLAGS_RELEASE
+            CMAKE_CXX_FLAGS_RELWITHDEBINFO
+            CMAKE_CXX_FLAGS_MINSIZEREL)
+            if(${flag_var} MATCHES "/MT")
+                string(REPLACE "/MT" "/MD" ${flag_var} "${${flag_var}}")
+            endif()
+            if(${flag_var} MATCHES "/MTd")
+                string(REPLACE "/MTd" "/MDd" ${flag_var} "${${flag_var}}")
+            endif()
+        endforeach()
     endif()
 endfunction()
--- scripts/build_and_test.py
+++ scripts/build_and_test.py
@@ -3,7 +3,7 @@ import json
 import subprocess
 import utils
 
-BUILD_TYPES = ['debug', 'release']
+BUILD_TYPES = ['debug-sanitized', 'debug-nonsanitized', 'release-sanitized', 'release-nonsanitized']
 
 parser = argparse.ArgumentParser(description='Build and test C/C++ code')
 parser.add_argument('-b', '--build-type', type=str, choices=BUILD_TYPES, help='Type of the build', default=None)
@@ -26,29 +26,30 @@ for build_type in requested_build_types:
     command = f'ctest --preset test-{build_type}'
     utils.run_command(command, shell=True, check=True)
 
-    if utils.program_available('valgrind'):
-        command = f'ctest --preset test-{build_type} --show-only=json-v1'
-        result = utils.run_command(command, shell=True, check=True, stdout=subprocess.PIPE, text=True)
+    if 'nonsanitized' in build_type:
+        if utils.program_available('valgrind'):
+            command = f'ctest --preset test-{build_type} --show-only=json-v1'
+            result = utils.run_command(command, shell=True, check=True, stdout=subprocess.PIPE, text=True)
 
-        tests_json = json.loads(result.stdout)
-        tests = tests_json.get('tests', [])
-        tests_executables_dict = {}  # Using dictionary and not set as iterating it preserves insertion order
-        for test in tests:
-            command = test.get('command', [])
-            if command:
-                tests_executables_dict[command[0]] = None
-            else:
-                print(f"No commands found for test {test.get('name')}")
-                exit(1)
-        for key in tests_executables_dict:
-            command = f'valgrind --error-exitcode=1 --leak-check=full {key}'
+            tests_json = json.loads(result.stdout)
+            tests = tests_json.get('tests', [])
+            tests_executables_dict = {}  # Using dictionary and not set as iterating it preserves insertion order
+            for test in tests:
+                command = test.get('command', [])
+                if command:
+                    tests_executables_dict[command[0]] = None
+                else:
+                    print(f"No commands found for test {test.get('name')}")
+                    exit(1)
+            for key in tests_executables_dict:
+                command = f'valgrind --error-exitcode=1 --leak-check=full {key}'
+                result = utils.run_command(command, shell=True, check=True)
+
+        if utils.program_available('clang-tidy') and not (utils.running_on_linux() and utils.running_on_github_actions()):
+            command = f'clang-tidy --version'
             result = utils.run_command(command, shell=True, check=True)
 
-    if utils.program_available('clang-tidy') and not (utils.running_on_linux() and utils.running_on_github_actions()):
-        command = f'clang-tidy --version'
-        result = utils.run_command(command, shell=True, check=True)
-
-        compilation_database_path = utils.PROJECT_DIR/'build'/build_type/'compile_commands.json'
-        script = utils.SCRIPTS_DIR/'run_clang_tidy.py'
-        command = f'{utils.PYTHON_EXECUTABLE} {script} -d {compilation_database_path} -e'
-        result = utils.run_command(command, shell=True, check=True)
+            compilation_database_path = utils.PROJECT_DIR/'build'/build_type/'compile_commands.json'
+            script = utils.SCRIPTS_DIR/'run_clang_tidy.py'
+            command = f'{utils.PYTHON_EXECUTABLE} {script} -d {compilation_database_path} -e'
+            result = utils.run_command(command, shell=True, check=True)
